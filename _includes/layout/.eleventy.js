be58a6d9df683b6149415ae64108c0dea02aec62
Your Name <you@example.com>
2023-07-03 17:05:27 +0000

    --wip-- [skip ci]

---
+++ _includes/layout/.eleventy.js
@@ -0,0 +1,9 @@
+/*
+ * .eleventy.js
+ * Copyright (C) 2023 mrubanov717 <mrubanov717@nilgroun>
+ *
+ * Distributed under terms of the MIT license.
+ */
+module.exports =function (config) {
+  config.addLayoutAlias('base', 'layout/base.ejs');
+}
